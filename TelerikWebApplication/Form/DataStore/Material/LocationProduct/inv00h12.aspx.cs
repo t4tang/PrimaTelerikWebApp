f85b335ae8644dbec35bda44ecb8695437d73d66
budakgaya <budakgaya@gmail.com>
2020-02-12 20:14:13 +0700

    Add new form

---
+++ TelerikWebApplication/Form/DataStore/Material/LocationProduct/inv00h12.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TelerikWebApplication.Form.DataStore.Material.LocationProduct
+{
+    public partial class inv00h12 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
