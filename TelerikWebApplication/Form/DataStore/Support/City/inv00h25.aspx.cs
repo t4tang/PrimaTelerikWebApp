703a83e2bcb10b5f254e82d2455c4e92e2057138
aidarase <ade_narase@yahoo.co.id>
2020-02-14 19:19:30 +0700

    Finish

--- TelerikWebApplication/Form/DataStore/Support/City/inv00h25.aspx.cs
+++ TelerikWebApplication/Form/DataStore/Support/City/inv00h25.aspx.cs
@@ -1,17 +1,190 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Telerik.Web.UI;
+using TelerikWebApplication.Class;
 
 namespace TelerikWebApplication.Form.DataStore.Support.City
 {
     public partial class inv00h25 : System.Web.UI.Page
     {
+        public static string koneksi = ConfigurationManager.ConnectionStrings["DbConString"].ConnectionString;
+        SqlConnection con = new SqlConnection(koneksi);
+        SqlDataAdapter sda = new SqlDataAdapter();
+        SqlCommand cmd = new SqlCommand();
+        private const int ItemsPerRequest = 10;
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void RadGrid1_NeedDataSource(object sender, Telerik.Web.UI.GridNeedDataSourceEventArgs e)
+        {
+            (sender as RadGrid).DataSource = GetDataTable();
+        }
+
+        public DataTable GetDataTable()
+        {
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "select inv00h25.city_code, inv00h25.city_name, inv00h20.prov_name from inv00h25 INNER JOIN inv00h20 ON inv00h25.prov_code = inv00h20.prov_code " +
+               " where inv00h25.stedit != 4";
+
+            cmd.CommandTimeout = 0;
+            cmd.ExecuteNonQuery();
+            sda = new SqlDataAdapter(cmd);
+            DataTable DT = new DataTable();
+
+            try
+            {
+                sda.Fill(DT);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return DT;
+        }
+
+        private static DataTable Getinv00h20(string text)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("select prov_code, prov_name from inv00h20 where prov_name like @text + '%'",
+            ConfigurationManager.ConnectionStrings["DbConString"].ConnectionString);
+            adapter.SelectCommand.Parameters.AddWithValue("@text", text);
+
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+
+            return data;
+
+        }
+
+        protected void RadGrid1_InsertCommand(object sender, Telerik.Web.UI.GridCommandEventArgs e)
+        {
+            GridEditableItem item = (GridEditableItem)e.Item;
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "INSERT INTO inv00h25(city_code, city_name, prov_code, Stamp,Usr,Owner,stEdit) VALUES (@city_code, @city_name, @prov_code, getdate(),@Usr, @Owner,0)";
+            cmd.Parameters.AddWithValue("@city_code", (item.FindControl("txt_city_code") as RadTextBox).Text);
+            cmd.Parameters.AddWithValue("@city_name", (item.FindControl("txt_city_name") as RadTextBox).Text);
+            cmd.Parameters.AddWithValue("@prov_code", (item.FindControl("cb_province") as RadComboBox).SelectedValue);
+            cmd.Parameters.AddWithValue("@Usr", public_str.user_id);
+            cmd.Parameters.AddWithValue("@Owner", public_str.user_id);
+            cmd.Parameters.AddWithValue("@status", "0");
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        protected void RadGrid1_UpdateCommand(object sender, Telerik.Web.UI.GridCommandEventArgs e)
+        {
+            GridEditFormItem item = (GridEditFormItem)e.Item;
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "UPDATE inv00h25 set city_name = @city_name, prov_code = @prov_code, LastUpdate = getdate(), Usr = @Usr where city_code = @city_code";
+            cmd.Parameters.AddWithValue("@city_name", (item.FindControl("txt_city_name") as RadTextBox).Text);
+            cmd.Parameters.AddWithValue("@city_code", (item.FindControl("txt_city_code") as RadTextBox).Text);
+            cmd.Parameters.AddWithValue("@prov_code", (item.FindControl("cb_province") as RadComboBox).SelectedValue);
+            cmd.Parameters.AddWithValue("@Usr", public_str.user_id);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        protected void RadGrid1_DeleteCommand(object sender, Telerik.Web.UI.GridCommandEventArgs e)
+        {
+            var productId = ((GridDataItem)e.Item).GetDataKeyValue("city_code");
+
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "update inv00h25 set stEdit = 4, LastUpdate = getdate(), Usr = @Usr where city_code = @city_code";
+            cmd.Parameters.AddWithValue("@city_code", productId);
+            cmd.Parameters.AddWithValue("@Usr", public_str.user_id);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        protected void RadGrid1_ItemCreated(object sender, Telerik.Web.UI.GridItemEventArgs e)
+        {
+            if (e.Item is GridEditableItem & e.Item.IsInEditMode)
+            {
+                GridEditFormItem item = (GridEditFormItem)e.Item;
+                RadTextBox txt = (item.FindControl("txt_city_code") as RadTextBox);
+                if (e.Item.OwnerTableView.IsItemInserted)
+                    txt.Enabled = true;
+                else
+                    txt.Enabled = false;
+            }
+        }
+
+        protected void cb_province_ItemsRequested(object sender, Telerik.Web.UI.RadComboBoxItemsRequestedEventArgs e)
+        {
+            DataTable data = Getprov_code(e.Text);
+
+            int itemOffset = e.NumberOfItems;
+            int endOffset = Math.Min(itemOffset + ItemsPerRequest, data.Rows.Count);
+            e.EndOfItems = endOffset == data.Rows.Count;
+
+            for (int i = itemOffset; i < endOffset; i++)
+            {
+                (sender as RadComboBox).Items.Add(new RadComboBoxItem(data.Rows[i]["prov_name"].ToString(), data.Rows[i]["prov_name"].ToString()));
+            }
+        }
+
+        private static DataTable Getprov_code(string text)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("select prov_code, prov_name from inv00h20 where prov_name like @text + '%'",
+            ConfigurationManager.ConnectionStrings["DbConString"].ConnectionString);
+            adapter.SelectCommand.Parameters.AddWithValue("@text", text);
+
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+
+            return data;
+
+        }
+
+        protected void cb_province_SelectedIndexChanged(object sender, Telerik.Web.UI.RadComboBoxSelectedIndexChangedEventArgs e)
+        {
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "select prov_code from inv00h20 where prov_name = '" + (sender as RadComboBox).Text + "'";
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+                (sender as RadComboBox).SelectedValue = dr["prov_code"].ToString();
+            dr.Close();
+            con.Close();
+        }
+
+        protected void cb_province_PreRender(object sender, EventArgs e)
+        {
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "select prov_code from inv00h20 where prov_name = '" + (sender as RadComboBox).Text + "'";
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+                (sender as RadComboBox).SelectedValue = dr["prov_code"].ToString();
+            dr.Close();
+            con.Close();
         }
     }
 }
