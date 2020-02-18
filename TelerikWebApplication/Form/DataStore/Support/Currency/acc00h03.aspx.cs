abf775bac337f3e402f56fee1c245a0f912bade4
Lempong11 <mario.lempong@yahoo.com>
2020-02-18 17:48:15 +0700

    no table reff

--- TelerikWebApplication/Form/DataStore/Support/Currency/acc00h03.aspx.cs
+++ TelerikWebApplication/Form/DataStore/Support/Currency/acc00h03.aspx.cs
@@ -1,17 +1,121 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Telerik.Web.UI;
+using TelerikWebApplication.Class;
 
 namespace TelerikWebApplication.Form.DataStore.Support.Currency
 {
     public partial class acc00h03 : System.Web.UI.Page
     {
+        public static string koneksi = ConfigurationManager.ConnectionStrings["DbConString"].ConnectionString;
+        SqlConnection con = new SqlConnection(koneksi);
+        SqlDataAdapter sda = new SqlDataAdapter();
+        SqlCommand cmd = new SqlCommand();
+        private const int ItemsPerRequest = 10;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public DataTable GetDataTable()
+        {
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT acc00h03.cur_name, acc00h03.disp_name, acc00h03.cur_code " +
+                                "FROM acc00h03 WHERE acc00h03.stEdit !=4";
+            cmd.CommandTimeout = 0;
+            cmd.ExecuteNonQuery();
+            sda = new SqlDataAdapter(cmd);
+
+            DataTable DT = new DataTable();
+            try
+            {
+                sda.Fill(DT);
+            }
+            finally
+            {
+                con.Close();
+            }
+            return DT;
+        }
+        protected void RadGrid1_NeedDataSource(object sender, Telerik.Web.UI.GridNeedDataSourceEventArgs e)
+        {
+            (sender as RadGrid).DataSource = GetDataTable();
+        }
+
+        protected void RadGrid1_DeleteCommand(object sender, Telerik.Web.UI.GridCommandEventArgs e)
+        {
+            var productId = ((GridDataItem)e.Item).GetDataKeyValue("cur_code");
+
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "Update acc00h03 SET stEdit = 4 where cur_code = @cur_code";
+            cmd.Parameters.AddWithValue("@cur_code", productId);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        protected void RadGrid1_UpdateCommand(object sender, Telerik.Web.UI.GridCommandEventArgs e)
+        {
+            if (e.Item is GridEditFormItem)
+            {
+                GridEditFormItem item = (GridEditFormItem)e.Item;
+                con.Open();
+                cmd = new SqlCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = con;
+                cmd.CommandText = "UPDATE acc00h03 SET cur_name = @cur_name, lastupdate = getdate(), userid = @userid, disp_name = @disp_name " +
+                                    "WHERE cur_code = @cur_code";
+                cmd.Parameters.AddWithValue("@cur_code", (item.FindControl("txt_cur_code") as RadTextBox).Text);
+                cmd.Parameters.AddWithValue("@cur_name", (item.FindControl("txt_cur_name") as RadTextBox).Text);
+                cmd.Parameters.AddWithValue("@userid", public_str.user_id);
+                cmd.Parameters.AddWithValue("@disp_name", (item.FindControl("txt_display") as RadTextBox).Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        protected void RadGrid1_InsertCommand(object sender, Telerik.Web.UI.GridCommandEventArgs e)
+        {
+            GridEditableItem item = (GridEditableItem)e.Item;
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "INSERT INTO acc00h03 (cur_code, cur_name, lastupdate, userid, stEdit, disp_name) " +
+                                "VALUES (@cur_code, @cur_name, getdate(), @userid, @stEdit, @disp_name)";
+            cmd.Parameters.AddWithValue("@cur_code", (item.FindControl("txt_cur_code") as RadTextBox).Text);
+            cmd.Parameters.AddWithValue("@cur_name", (item.FindControl("txt_cur_name") as RadTextBox).Text);
+            cmd.Parameters.AddWithValue("@userid", public_str.user_id);
+            cmd.Parameters.AddWithValue("@stEdit", "0");
+            cmd.Parameters.AddWithValue("@disp_name", (item.FindControl("txt_display") as RadTextBox).Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        protected void RadGrid1_ItemCreated(object sender, Telerik.Web.UI.GridItemEventArgs e)
+        {
+            if (e.Item is GridEditableItem & e.Item.IsInEditMode)
+            {
+                GridEditFormItem item = (GridEditFormItem)e.Item;
+                RadTextBox txt = (item.FindControl("txt_cur_code") as RadTextBox);
+                if (e.Item.OwnerTableView.IsItemInserted)
+                    txt.Enabled = true;
+                else
+                    txt.Enabled = false;
+            }
         }
     }
 }
