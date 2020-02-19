c43178f46789f6e66e47fc33113af56d77359203
budakgaya <budakgaya@gmail.com>
2020-02-19 15:12:14 +0700

    New Form

---
+++ TelerikWebApplication/Form/DataStore/Support/OtherExpense/acc00h28.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TelerikWebApplication.Form.DataStore.Support.OtherExpense
+{
+    public partial class acc00h28 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
