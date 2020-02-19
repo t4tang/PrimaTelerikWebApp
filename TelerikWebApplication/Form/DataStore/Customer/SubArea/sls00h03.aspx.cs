a68d9856b1fddec5a10ade0445d0f75192126891
budakgaya <budakgaya@gmail.com>
2020-02-19 11:09:00 +0700

    New Forms

---
+++ TelerikWebApplication/Form/DataStore/Customer/SubArea/sls00h03.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TelerikWebApplication.Form.DataStore.Customer.SubArea
+{
+    public partial class sls00h03 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
