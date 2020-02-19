2c7813f9dc18f37592951ca7123170f229f998e4
aidarase <ade_narase@yahoo.co.id>
2020-02-19 18:55:14 +0700

    Finish

--- TelerikWebApplication/Form/DataStore/Supplier/SupplierGroup/acc00h08.aspx
+++ TelerikWebApplication/Form/DataStore/Supplier/SupplierGroup/acc00h08.aspx
@@ -1,7 +1,155 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="acc00h08.aspx.cs" Inherits="TelerikWebApplication.Form.DataStore.Supplier.SupplierGroup.acc00h08" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="../../../../Styles/common.css" rel="stylesheet" />
+    <link href="../../../../Styles/mail.css" rel="stylesheet" />
+    <link href="../../../../Styles/custom-cs.css" rel="stylesheet" />
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="FolderContent" runat="server">
+    <nav:FolderNavigationControl runat="server" ID="FolderNavigationControl" />
+    <nav:MobileNavigation runat="server" ID="MobileNavigation"></nav:MobileNavigation>
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+     <telerik:RadAjaxManager ID="RadAjaxManager1" runat="server">
+        <AjaxSettings>
+            <telerik:AjaxSetting AjaxControlID="RadGrid1">
+                <UpdatedControls>
+                    <telerik:AjaxUpdatedControl ControlID="RadGrid1" LoadingPanelID="RadAjaxLoadingPanel1"></telerik:AjaxUpdatedControl>
+                </UpdatedControls>
+            </telerik:AjaxSetting>
+            <telerik:AjaxSetting AjaxControlID="ConfiguratorPanel">
+                <UpdatedControls>
+                    <telerik:AjaxUpdatedControl ControlID="RadGrid1" LoadingPanelID="RadAjaxLoadingPanel1"></telerik:AjaxUpdatedControl>
+                </UpdatedControls>
+            </telerik:AjaxSetting>
+        </AjaxSettings>
+    </telerik:RadAjaxManager>
+
+    <telerik:RadAjaxLoadingPanel ID="RadAjaxLoadingPanel1" runat="server">
+    </telerik:RadAjaxLoadingPanel>
+    <div class="scroller">
+        <telerik:RadGrid ID="RadGrid1" runat="server" RenderMode="Lightweight" AllowPaging="True" 
+            ShowFooter ="true" 
+            AutoGenerateColumns="False" MasterTableView-AutoGenerateColumns="False"
+             OnNeedDataSource ="RadGrid1_NeedDataSource" OnInsertCommand ="RadGrid1_InsertCommand" OnUpdateCommand ="RadGrid1_UpdateCommand"
+            OnDeleteCommand ="RadGrid1_DeleteCommand" Skin ="MetroTouch" OnItemCreated ="RadGrid1_ItemCreated"
+             MasterTableView-CommandItemDisplay="Top" MasterTableView-DataKeyNames="KoGSup" MasterTableView-ClientDataKeyNames="KoGSup" 
+            MasterTableView-AllowFilteringByColumn="True" AllowSorting="True">
+            <MasterTableView>
+                <Columns>
+          
+                    <telerik:GridEditCommandColumn UniqueName ="EditCommandColumn">
+                        <HeaderStyle Width ="15px" ></HeaderStyle>
+                        <ItemStyle Width = "15px" />
+                    </telerik:GridEditCommandColumn>
+                     <telerik:GridBoundColumn HeaderText ="Code" DataField ="KoGSup" >
+                        <HeaderStyle Width ="100px" > </HeaderStyle>
+                    </telerik:GridBoundColumn>
+                     <telerik:GridBoundColumn HeaderText ="Supplier Group" DataField ="NmGSup" >
+                        <HeaderStyle Width ="420px" > </HeaderStyle>
+                    </telerik:GridBoundColumn>
+                     <telerik:GridBoundColumn HeaderText ="Currency" DataField ="cur_name" >
+                        <HeaderStyle Width ="420px" > </HeaderStyle>
+                    </telerik:GridBoundColumn>
+                    
+                    <telerik:GridButtonColumn UniqueName ="DeleteColumn" Text ="Delete" CommandName="Delete" HeaderStyle-Width="30px"
+                        ConfirmText="Delete This Product?" ConfirmDialogType="RadWindow" ConfirmTitle="Delete" ButtonType="FontIconButton">
+                        <HeaderStyle Width="30px"></HeaderStyle>
+                    </telerik:GridButtonColumn>
+                </Columns>
+                <EditFormSettings EditFormType="Template">
+                    <FormTemplate>
+                          <table id="Table2" cellspacing="4" cellpadding="5" width="100%" border="0" rules="none" class="formTemplate_Table">
+                            <tr>
+                                <td>
+                                    Code:
+                                </td>
+                                <td>
+                                    <telerik:RadTextBox ID="txt_KoGSup" runat="server" Width="50px" Enabled="true"
+                                        RenderMode="Lightweight" Text='<%# DataBinder.Eval(Container, "DataItem.KoGSup") %>' AutoPostBack="false"></telerik:RadTextBox>
+
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Supplier Group:
+                                </td>
+                                <td>
+                                    <telerik:RadTextBox ID="txt_NmGSup" runat="server" Width="250px" Enabled="true"
+                                        RenderMode="Lightweight" Text='<%# DataBinder.Eval(Container, "DataItem.NmGSup") %>'  AutoPostBack="false"></telerik:RadTextBox>
+                                </td>
+                            </tr>
+                               <tr>
+                                <td>
+                                    Currency:
+                                </td>
+                                <td>
+                                    <telerik:RadComboBox ID="cb_currency" runat="server" RenderMode="Lightweight" Width ="150px"
+                                        Text='<%# DataBinder.Eval(Container, "DataItem.cur_name") %>' 
+                                         OnItemsRequested ="cb_currency_ItemsRequested" OnSelectedIndexChanged ="cb_currency_SelectedIndexChanged"
+                                        EnableLoadOnDemand="true" ShowDropDownOnTextboxClick="true" OnPreRender ="cb_currency_PreRender"
+                                        EnableVirtualScrolling="true" ShowMoreResultsBox="true"
+                                        AutoPostBack="false" Skin ="MetroTouch"
+                                        Height="200" MarkFirstMatch="true">
+                                    </telerik:RadComboBox>
+                                </td>
+                            </tr>                  
+                              <tr>
+                                <td>
+                                    Acc. Payable:
+                                </td>
+                                <td>
+                                    <telerik:RadComboBox ID="cb_payable" runat="server" RenderMode="Lightweight" Width ="350px"
+                                        Text='<%# DataBinder.Eval(Container, "DataItem.korekname") %>' 
+                                         OnItemsRequested ="cb_payable_ItemsRequested" OnSelectedIndexChanged ="cb_payable_SelectedIndexChanged"
+                                        EnableLoadOnDemand="true" ShowDropDownOnTextboxClick="true" OnPreRender ="cb_payable_PreRender"
+                                        EnableVirtualScrolling="true" ShowMoreResultsBox="true"
+                                        AutoPostBack="false" Skin ="MetroTouch"
+                                        Height="200" MarkFirstMatch="true">
+                                    </telerik:RadComboBox>
+                                </td>
+                            </tr>                  
+                             <tr>
+                                <td>
+                                    AP Accrued:
+                                </td>
+                                <td>
+                                    <telerik:RadComboBox ID="cb_accrued" runat="server" RenderMode="Lightweight" Width ="350px"
+                                        Text='<%# DataBinder.Eval(Container, "DataItem.umname") %>' 
+                                         OnItemsRequested ="cb_accrued_ItemsRequested" OnSelectedIndexChanged ="cb_accrued_SelectedIndexChanged"
+                                        EnableLoadOnDemand="true" ShowDropDownOnTextboxClick="true" OnPreRender ="cb_accrued_PreRender"
+                                        EnableVirtualScrolling="true" ShowMoreResultsBox="true"
+                                        AutoPostBack="false" Skin ="MetroTouch"
+                                        Height="200" MarkFirstMatch="true">
+                                    </telerik:RadComboBox>
+                                </td>
+                            </tr>                  
+                             <tr>
+                                <td>
+                                    DP:
+                                </td>
+                                <td>
+                                    <telerik:RadComboBox ID="cb_dp" runat="server" RenderMode="Lightweight" Width ="350px"
+                                        Text='<%# DataBinder.Eval(Container, "DataItem.Expensename") %>' 
+                                         OnItemsRequested ="cb_dp_ItemsRequested" OnSelectedIndexChanged ="cb_dp_SelectedIndexChanged"
+                                        EnableLoadOnDemand="true" ShowDropDownOnTextboxClick="true" OnPreRender ="cb_dp_PreRender"
+                                        EnableVirtualScrolling="true" ShowMoreResultsBox="true"
+                                        AutoPostBack="false" Skin ="MetroTouch"
+                                        Height="200" MarkFirstMatch="true">
+                                    </telerik:RadComboBox>
+                                </td>
+                            </tr>                       
+                            <tr >
+                                <td align="right" colspan="2">
+                                    <asp:Button ID="btnUpdate" Text='<%# (Container is GridEditFormInsertItem) ? "Insert" : "Update" %>'
+                                    runat ="server" CommandName ='<%# (Container is GridEditFormInsertItem) ? "PerformInsert" : "Update" %>' >
+                                        </asp:Button>&nbsp;
+                                    <asp:Button ID ="btnCancel" Text="Cancel" runat="server" CausesValidation ="false" CommandName="Cancel"> </asp:Button>
+                                </td>
+                            </tr>
+                        </table> 
+                    </FormTemplate>
+                </EditFormSettings>     
+            </MasterTableView>
+        </telerik:RadGrid>
+   </div>
 </asp:Content>
--- TelerikWebApplication/Form/DataStore/Supplier/SupplierGroup/acc00h08.aspx.cs
+++ TelerikWebApplication/Form/DataStore/Supplier/SupplierGroup/acc00h08.aspx.cs
@@ -1,17 +1,386 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Telerik.Web.UI;
+using TelerikWebApplication.Class;
 
 namespace TelerikWebApplication.Form.DataStore.Supplier.SupplierGroup
 {
     public partial class acc00h08 : System.Web.UI.Page
     {
+        public static string koneksi = ConfigurationManager.ConnectionStrings["DbConString"].ConnectionString;
+        SqlConnection con = new SqlConnection(koneksi);
+        SqlDataAdapter sda = new SqlDataAdapter();
+        SqlCommand cmd = new SqlCommand();
+        private const int ItemsPerRequest = 10;
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        private static string GetStatusMessage(int offset, int total)
+        {
+            if (total <= 0)
+                return "No matches";
+
+            return String.Format("Items <b>1</b>-<b>{0}</b> out of <b>{1}</b>", offset, total);
+        }
+        protected void RadGrid1_NeedDataSource(object sender, Telerik.Web.UI.GridNeedDataSourceEventArgs e)
+        {
+            (sender as RadGrid).DataSource = GetDataTable();
+        }
+
+        public DataTable GetDataTable()
+        {
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "select acc00h08.KoGSup, acc00h08.NmGSup, acc00h03.cur_name, acc00h08.korek + ' ' + acc00h10.accountname as korekname, acc00h08.um + ' ' + acc00h10.accountname as umname, acc00h08.Expense + ' ' + acc00h10.accountname as Expensename " +
+                "from acc00h08 INNER JOIN acc00h10 ON acc00h10.accountno = acc00h08.korek " +
+               " INNER JOIN acc00h03 ON acc00h03.cur_code = acc00h08.cur_code where acc00h08.stedit != 4  ";
+
+            cmd.CommandTimeout = 0;
+            cmd.ExecuteNonQuery();
+            sda = new SqlDataAdapter(cmd);
+            DataTable DT = new DataTable();
+
+            try
+            {
+                sda.Fill(DT);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return DT;
+        }
+
+        private static DataTable Getacc00h03(string text)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("select cur_code, cur_name from acc00h03 where cur_code like @text + '%'",
+            ConfigurationManager.ConnectionStrings["DbConString"].ConnectionString);
+            adapter.SelectCommand.Parameters.AddWithValue("@text", text);
+
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+
+            return data;
+
+        }
+
+        private static DataTable Getacc00h10(string text)
+        {
+            
+            SqlDataAdapter adapter = new SqlDataAdapter("select accountno, accountno + ' ' + accountname as accountname from acc00h10 where accountno like @text + '%'",
+            ConfigurationManager.ConnectionStrings["DbConString"].ConnectionString);
+            adapter.SelectCommand.Parameters.AddWithValue("@text", text);
+
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+
+            return data;
+
+        }
+        protected void RadGrid1_InsertCommand(object sender, Telerik.Web.UI.GridCommandEventArgs e)
+        {
+            GridEditableItem item = (GridEditableItem)e.Item;
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "INSERT INTO acc00h08(KoGSup, NmGSup, korek, um, Expense, cur_code, Stamp,Usr,Owner,stEdit) VALUES (@KoGSup, @NmGSup, @korek, @um, @Expense, @cur_code, getdate(),UPPER(@Usr),UPPER (@Owner),0)";
+            cmd.Parameters.AddWithValue("@KoGSup", (item.FindControl("txt_KoGSup") as RadTextBox).Text);
+            cmd.Parameters.AddWithValue("@NmGSup", (item.FindControl("txt_NmGSup") as RadTextBox).Text);
+            cmd.Parameters.AddWithValue("@korek", (item.FindControl("cb_payable") as RadComboBox).SelectedValue);
+            cmd.Parameters.AddWithValue("@um", (item.FindControl("cb_accrued") as RadComboBox).SelectedValue);
+            cmd.Parameters.AddWithValue("@Expense", (item.FindControl("cb_dp") as RadComboBox).SelectedValue);
+            cmd.Parameters.AddWithValue("@cur_code", (item.FindControl("cb_currency") as RadComboBox).SelectedValue);
+            cmd.Parameters.AddWithValue("@Usr", public_str.user_id);
+            cmd.Parameters.AddWithValue("@Owner", public_str.user_id);
+            cmd.Parameters.AddWithValue("@status", "0");
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        protected void RadGrid1_UpdateCommand(object sender, Telerik.Web.UI.GridCommandEventArgs e)
+        {
+            GridEditFormItem item = (GridEditFormItem)e.Item;
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "UPDATE acc00h08 set KoGSup = @KoGSup, NmGSup = @NmGSup, korek = @korek, um = @um, Expense = @Expense, cur_code = @cur_code, LastUpdate = getdate(), Usr = UPPER(@Usr) where KoGSup = @KoGSup";
+            cmd.Parameters.AddWithValue("@KoGSup", (item.FindControl("txt_KoGSup") as RadTextBox).Text);
+            cmd.Parameters.AddWithValue("@NmGSup", (item.FindControl("txt_NmGSup") as RadTextBox).Text);
+            cmd.Parameters.AddWithValue("@korek", (item.FindControl("cb_payable") as RadComboBox).SelectedValue);
+            cmd.Parameters.AddWithValue("@um", (item.FindControl("cb_accrued") as RadComboBox).SelectedValue);
+            cmd.Parameters.AddWithValue("@Expense", (item.FindControl("cb_dp") as RadComboBox).SelectedValue);
+            cmd.Parameters.AddWithValue("@cur_code", (item.FindControl("cb_currency") as RadComboBox).SelectedValue);
+            cmd.Parameters.AddWithValue("@Usr", public_str.user_id);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        protected void RadGrid1_DeleteCommand(object sender, Telerik.Web.UI.GridCommandEventArgs e)
+        {
+            var productId = ((GridDataItem)e.Item).GetDataKeyValue("KoGSup");
+
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "update acc00h08 set stEdit = 4, LastUpdate = getdate(), Usr = @Usr where KoGSup = @KoGSup";
+            cmd.Parameters.AddWithValue("@KoGSup", productId);
+            cmd.Parameters.AddWithValue("@Usr", public_str.user_id);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        protected void RadGrid1_ItemCreated(object sender, Telerik.Web.UI.GridItemEventArgs e)
+        {
+            if (e.Item is GridEditableItem & e.Item.IsInEditMode)
+            {
+                GridEditFormItem item = (GridEditFormItem)e.Item;
+                RadTextBox txt = (item.FindControl("txt_KoGSup") as RadTextBox);
+                if (e.Item.OwnerTableView.IsItemInserted)
+                    txt.Enabled = true;
+                else
+                    txt.Enabled = false;
+            }
+        }
+
+        protected void cb_currency_ItemsRequested(object sender, RadComboBoxItemsRequestedEventArgs e)
+        {
+            DataTable data = Getaccountcurrency(e.Text);
+
+            int itemOffset = e.NumberOfItems;
+            int endOffset = Math.Min(itemOffset + ItemsPerRequest, data.Rows.Count);
+            e.EndOfItems = endOffset == data.Rows.Count;
+
+            for (int i = itemOffset; i < endOffset; i++)
+            {
+                (sender as RadComboBox).Items.Add(new RadComboBoxItem(data.Rows[i]["cur_name"].ToString(), data.Rows[i]["cur_name"].ToString()));
+            }
+        }
+
+        private static DataTable Getaccountcurrency(string text)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("select cur_code, cur_name from acc00h03 where cur_code like @text + '%'",
+            ConfigurationManager.ConnectionStrings["DbConString"].ConnectionString);
+            adapter.SelectCommand.Parameters.AddWithValue("@text", text);
+
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+
+            return data;
+
+        }
+        protected void cb_currency_SelectedIndexChanged(object sender, RadComboBoxSelectedIndexChangedEventArgs e)
+        {
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "select cur_code from acc00h03 where cur_name = '" + (sender as RadComboBox).Text + "'";
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+                (sender as RadComboBox).SelectedValue = dr["cur_code"].ToString();
+            dr.Close();
+            con.Close();
+        }
+
+        protected void cb_currency_PreRender(object sender, EventArgs e)
+        {
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "select cur_code from acc00h03 where cur_name = '" + (sender as RadComboBox).Text + "'";
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+                (sender as RadComboBox).SelectedValue = dr["cur_code"].ToString();
+            dr.Close();
+            con.Close();
+        }
+
+        protected void cb_payable_ItemsRequested(object sender, RadComboBoxItemsRequestedEventArgs e)
+        {
+            DataTable data = GetKorek(e.Text);
+
+            int itemOffset = e.NumberOfItems;
+            int endOffset = Math.Min(itemOffset + ItemsPerRequest, data.Rows.Count);
+            e.EndOfItems = endOffset == data.Rows.Count;
+
+            for (int i = itemOffset; i < endOffset; i++)
+            {
+                (sender as RadComboBox).Items.Add(new RadComboBoxItem(data.Rows[i]["accountname"].ToString(), data.Rows[i]["accountname"].ToString()));
+            }
+        }
+
+        private static DataTable GetKorek(string text)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("select accountno +' '+ accountname as accountname from acc00h10 where stEdit != 4 " +
+                " AND accountno +' '+ accountname LIKE @text + '%'",
+            ConfigurationManager.ConnectionStrings["DbConString"].ConnectionString);
+            adapter.SelectCommand.Parameters.AddWithValue("@text", text);
+
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+
+            return data;
+
+        }
+        protected void cb_payable_SelectedIndexChanged(object sender, RadComboBoxSelectedIndexChangedEventArgs e)
+        {
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT * FROM acc00h10 WHERE accountno +' '+ accountname = '" + (sender as RadComboBox).Text + "'";
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+                (sender as RadComboBox).SelectedValue = dr["accountno"].ToString();
+            dr.Close();
+            con.Close();
+        }
+
+        protected void cb_payable_PreRender(object sender, EventArgs e)
+        {
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT accountno FROM acc00h10 WHERE accountno +' '+ accountname = '" + (sender as RadComboBox).Text + "'";
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+                (sender as RadComboBox).SelectedValue = dr["accountno"].ToString();
+            dr.Close();
+            con.Close();
+        }
+
+        protected void cb_accrued_ItemsRequested(object sender, RadComboBoxItemsRequestedEventArgs e)
+        {
+            DataTable data = Getum(e.Text);
+
+            int itemOffset = e.NumberOfItems;
+            int endOffset = Math.Min(itemOffset + ItemsPerRequest, data.Rows.Count);
+            e.EndOfItems = endOffset == data.Rows.Count;
+
+            for (int i = itemOffset; i < endOffset; i++)
+            {
+                (sender as RadComboBox).Items.Add(new RadComboBoxItem(data.Rows[i]["accountname"].ToString(), data.Rows[i]["accountname"].ToString()));
+            }
+        }
+
+        private static DataTable Getum(string text)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("select accountno +' '+ accountname as accountname from acc00h10 where stEdit != 4 " +
+                " AND accountno +' '+ accountname LIKE @text + '%'",
+            ConfigurationManager.ConnectionStrings["DbConString"].ConnectionString);
+            adapter.SelectCommand.Parameters.AddWithValue("@text", text);
+
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+
+            return data;
+
+        }
+        protected void cb_accrued_SelectedIndexChanged(object sender, RadComboBoxSelectedIndexChangedEventArgs e)
+        {
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "select accountno from acc00h10 where accountno +' '+ accountname = '" + (sender as RadComboBox).Text + "'";
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+                (sender as RadComboBox).SelectedValue = dr["accountno"].ToString();
+            dr.Close();
+            con.Close();
+        }
+
+        protected void cb_accrued_PreRender(object sender, EventArgs e)
+        {
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "select accountno from acc00h10 where accountno +' '+ accountname = '" + (sender as RadComboBox).Text + "'";
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+                (sender as RadComboBox).SelectedValue = dr["accountno"].ToString();
+            dr.Close();
+            con.Close();
+        }
+
+        protected void cb_dp_ItemsRequested(object sender, RadComboBoxItemsRequestedEventArgs e)
+        {
+            DataTable data = GetExpense(e.Text);
+
+            int itemOffset = e.NumberOfItems;
+            int endOffset = Math.Min(itemOffset + ItemsPerRequest, data.Rows.Count);
+            e.EndOfItems = endOffset == data.Rows.Count;
+
+            for (int i = itemOffset; i < endOffset; i++)
+            {
+                (sender as RadComboBox).Items.Add(new RadComboBoxItem(data.Rows[i]["accountname"].ToString(), data.Rows[i]["accountname"].ToString()));
+            }
+        }
+
+        private static DataTable GetExpense(string text)
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("select accountno +' '+ accountname as accountname from acc00h10 where stEdit != 4 " +
+                " AND accountno +' '+ accountname LIKE @text + '%'",
+            ConfigurationManager.ConnectionStrings["DbConString"].ConnectionString);
+            adapter.SelectCommand.Parameters.AddWithValue("@text", text);
+
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+
+            return data;
+
+        }
+        protected void cb_dp_SelectedIndexChanged(object sender, RadComboBoxSelectedIndexChangedEventArgs e)
+        {
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "select accountno from acc00h10 where accountno +' '+ accountname = '" + (sender as RadComboBox).Text + "'";
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+                (sender as RadComboBox).SelectedValue = dr["accountno"].ToString();
+            dr.Close();
+            con.Close();
+        }
+
+        protected void cb_dp_PreRender(object sender, EventArgs e)
+        {
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "select accountno from acc00h10 where accountno +' '+ accountname = '" + (sender as RadComboBox).Text + "'";
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+                (sender as RadComboBox).SelectedValue = dr["accountno"].ToString();
+            dr.Close();
+            con.Close();
         }
     }
 }
