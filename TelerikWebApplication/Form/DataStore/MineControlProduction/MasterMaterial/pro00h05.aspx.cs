e0e5852721b69d7c96c4030e303d5e5f1f4a974b
budakgaya <budakgaya@gmail.com>
2020-02-26 15:21:03 +0700

    New Form

---
+++ TelerikWebApplication/Form/DataStore/MineControlProduction/MasterMaterial/pro00h05.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TelerikWebApplication.Form.DataStore.MineControlProduction.MasterMaterial
+{
+    public partial class pro00h05 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
