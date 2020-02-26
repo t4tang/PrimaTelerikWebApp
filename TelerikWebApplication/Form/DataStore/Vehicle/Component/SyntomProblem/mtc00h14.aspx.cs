996b5f354d98da4770261e4fc41ae846babb0b57
viirfan <yami1927@yahoo.com>
2020-02-26 11:32:28 +0700

    done

--- TelerikWebApplication/Form/DataStore/Vehicle/Component/SyntomProblem/mtc00h14.aspx.cs
+++ TelerikWebApplication/Form/DataStore/Vehicle/Component/SyntomProblem/mtc00h14.aspx.cs
@@ -1,17 +1,125 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Telerik.Web.UI;
+using TelerikWebApplication.Class;
 
 namespace TelerikWebApplication.Form.DataStore.Vehicle.Component.SyntomProblem
 {
     public partial class mtc00h14 : System.Web.UI.Page
     {
+        public static string koneksi = ConfigurationManager.ConnectionStrings["DbConString"].ConnectionString;
+        SqlConnection con = new SqlConnection(koneksi);
+        SqlDataAdapter sda = new SqlDataAdapter();
+        SqlCommand cmd = new SqlCommand();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void RadAjaxManager1_AjaxRequest(object sender, Telerik.Web.UI.AjaxRequestEventArgs e)
+        {
+
+        }
+
+        private static string GetStatusMessage(int offset, int total)
+        {
+            if (total <= 0)
+                return "No matches";
+
+            return String.Format("Items <b>1</b>-<b>{0}</b> out of <b>{1}</b>", offset, total);
+        }
+        public DataTable GetDataTable()
+        {
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT sym_code, sym_name, Usr, stEdit FROM mtc00h14 WHERE stEdit !='4'";
+            cmd.CommandTimeout = 0;
+            cmd.ExecuteNonQuery();
+            sda = new SqlDataAdapter(cmd);
+
+            DataTable DT = new DataTable();
+
+            try
+            {
+                sda.Fill(DT);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return DT;
+        }
+        protected void RadGrid1_NeedDataSource(object sender, Telerik.Web.UI.GridNeedDataSourceEventArgs e)
+        {
+            (sender as RadGrid).DataSource = GetDataTable();
+        }
+
+        protected void RadGrid1_InsertCommand(object sender, Telerik.Web.UI.GridCommandEventArgs e)
+        {
+            GridEditableItem item = (GridEditableItem)e.Item;
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "INSERT INTO mtc00h14 (sym_code, sym_name, Usr, stEdit) VALUES " +
+                              "(@sym_code, @sym_name, @Usr, '0')";
+            cmd.Parameters.AddWithValue("@sym_code", (item.FindControl("txt_code") as RadTextBox).Text);
+            cmd.Parameters.AddWithValue("@sym_name", (item.FindControl("txt_name") as RadTextBox).Text);
+            cmd.Parameters.AddWithValue("@Usr", public_str.user_id);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        protected void RadGrid1_UpdateCommand(object sender, Telerik.Web.UI.GridCommandEventArgs e)
+        {
+            GridEditableItem item = (GridEditableItem)e.Item;
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "UPDATE mtc00h14 SET sym_name = @sym_name, Usr = @Usr WHERE sym_code = @sym_code";
+            cmd.Parameters.AddWithValue("@sym_code", (item.FindControl("txt_code") as RadTextBox).Text);
+            cmd.Parameters.AddWithValue("@sym_name", (item.FindControl("txt_name") as RadTextBox).Text);
+            cmd.Parameters.AddWithValue("@Usr", public_str.user_id);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        protected void RadGrid1_DeleteCommand(object sender, Telerik.Web.UI.GridCommandEventArgs e)
+        {
+            var sym_code = ((GridDataItem)e.Item).GetDataKeyValue("sym_code");
+
+            con.Open();
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "UPDATE mtc00h14 SET stEdit = 4 where sym_code = @sym_code";
+            cmd.Parameters.AddWithValue("@sym_code", sym_code);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        protected void RadGrid1_ItemCreated(object sender, Telerik.Web.UI.GridItemEventArgs e)
+        {
+            if (e.Item is GridEditableItem & e.Item.IsInEditMode)
+            {
+                GridEditFormItem item = (GridEditFormItem)e.Item;
+                RadTextBox txt = (item.FindControl("txt_code") as RadTextBox);
+                if (e.Item.OwnerTableView.IsItemInserted)
+                    txt.Enabled = true;
+                else
+                    txt.Enabled = false;
+            }
         }
     }
 }
