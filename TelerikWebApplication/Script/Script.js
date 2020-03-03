b37c65a24dba619e1e8f3ac717fcb15d2af88090
budakgaya <budakgaya@gmail.com>
2020-03-03 15:53:45 +0700

    New js

---
+++ TelerikWebApplication/Script/Script.js
@@ -0,0 +1,21 @@
+﻿(function (global, undefined) {
+    var demo = {};
+
+    function populateValue() {
+        $get(demo.label).innerHTML = $get(demo.textBox).value;
+        //the RadWindow's content template is an INaming container and the server code block is needed
+        $find(demo.contentTemplateID).close();
+    }
+
+    function openWinContentTemplate() {
+        $find(demo.templateWindowID).show();
+    }
+    function openWinNavigateUrl() {
+        $find(demo.urlWindowID).show();
+    }
+
+    global.$windowContentDemo = demo;
+    global.populateValue = populateValue;
+    global.openWinContentTemplate = openWinContentTemplate;
+    global.openWinNavigateUrl = openWinNavigateUrl;
+})(window);
