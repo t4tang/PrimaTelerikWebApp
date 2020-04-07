4f6cbe555c61b53335e6447cff36556e7d3b0513
aidha_rase <ade_narase@yahoo.co.id>
2020-04-07 17:06:33 +0700

    4-7-2020

--- TelerikWebApplication/Form/Fico/Bank/BankMutation/acc01h01.aspx.designer.cs
+++ TelerikWebApplication/Form/Fico/Bank/BankMutation/acc01h01.aspx.designer.cs
@@ -166,6 +166,15 @@ namespace TelerikWebApplication.Form.Fico.Bank.BankMutation {
         protected global::System.Web.UI.WebControls.ImageButton btnSave;
         
         /// <summary>
+        /// btnPrint control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton btnPrint;
+        
+        /// <summary>
         /// lbl_form_name control.
         /// </summary>
         /// <remarks>
