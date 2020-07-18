a235eded3e49f9086b80888f559057ca18066ac3
budakgaya <budakgaya@gmail.com>
2020-07-19 05:59:21 +0700

    New Forms GTI & GTO

---
+++ TelerikWebApplication/Form/Inventory/GoodsTransfer/InComing/inv01h04gtiEditForm.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TelerikWebApplication.Form.Inventory.GoodsTransfer.InComing
+{
+    public partial class inv01h04gtiEditForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
