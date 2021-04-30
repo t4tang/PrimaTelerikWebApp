959e98b91fece7336c517b53a9143c933e9359a9
aidarase <ade_narase@yahoo.co.id>
2021-04-30 15:54:38 +0700

    30042021

---
+++ TelerikWebApplication/Form/Fico/Bank/PreBankPayment/acc01h20_slip.cs
@@ -0,0 +1,27 @@
+namespace TelerikWebApplication.Form.Fico.Bank.PreBankPayment
+{
+    using System;
+    using System.ComponentModel;
+    using System.Drawing;
+    using System.Windows.Forms;
+    using Telerik.Reporting;
+    using Telerik.Reporting.Drawing;
+
+    /// <summary>
+    /// Summary description for acc01h20_slip.
+    /// </summary>
+    public partial class acc01h20_slip : Telerik.Reporting.Report
+    {
+        public acc01h20_slip()
+        {
+            //
+            // Required for telerik Reporting designer support
+            //
+            InitializeComponent();
+
+            //
+            // TODO: Add any constructor code after InitializeComponent call
+            //
+        }
+    }
+}
